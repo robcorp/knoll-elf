38c1d9c7308ecdc53e9103507863c348ad383030
Robert Houser <robcorp@mac.com>
2019-01-28 15:39:28 -0500

    Updated db to include lead time filter state and events to deal with changing lead time filter states.

--- src/cljs/elf/db.cljs
+++ src/cljs/elf/db.cljs
@@ -3,4 +3,8 @@
 
 (def default-db
   {:name "re-frame"
-   :essentials-products dd/essentials-products})
+   :essentials-products dd/essentials-products
+   :filter-all-lead-times? true
+   :filter-quick-ship-lead-times? false
+   :filter-three-week-ship-lead-times? false
+   :filter-standard-ship-lead-times? false})
--- src/cljs/elf/events.cljs
+++ src/cljs/elf/events.cljs
@@ -9,3 +9,17 @@
  ::initialize-db
  (fn-traced [_ _]
    db/default-db))
+
+(defn toggle-filter-state [db filter]
+  (assoc db filter (not (filter db))))
+
+(re-frame/reg-event-db
+ ::check-box-clicked
+ (fn [db event]
+   (let [[_ id] event]
+     #_(println id "checkbox was clicked.")
+     (cond
+       (= id "all-lead") (toggle-filter-state db :filter-all-lead-times?)
+       (= id "standard-ship") (toggle-filter-state db :filter-standard-ship-lead-times?)
+       (= id "three-week-ship") (toggle-filter-state db :filter-three-week-ship-lead-times?)
+       (= id "quick-ship") (toggle-filter-state db :filter-quick-ship-lead-times?)))))
