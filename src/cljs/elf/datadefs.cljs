2a1c5d9d89070a75e90d095ece39f9e2d7570a2c
Robert Houser <robcorp@mac.com>
2019-01-28 15:38:12 -0500

    Modified data structure to support product types

--- src/cljs/elf/datadefs.cljs
+++ src/cljs/elf/datadefs.cljs
@@ -15,7 +15,21 @@
 
 
 (def essentials-products
-  [{:product-id :barcelona-chair, :product-name "Barcelona Chair" :lead-times [:std]}
-   {:product-id :pollock-chair, :product-name "Pollock Chair" :lead-times [:quick]}
-   {:product-id :wassily-chair, :product-name "Wassily Chair" :lead-times [:three-week]}
-   ])
+  [{:product-type "Lounge Chairs"
+    :product-list [
+                   {:product-id :barcelona-chair, :product-name "Barcelona Chair" :lead-times [:std :quick :three-week] :img-src "https://www.knoll.com/media/858/141/205_barcelona_lounge_black_F_v3.png"}
+                   {:product-id :pollock-chair, :product-name "Pollock Chair" :lead-times [:quick :std] :img-src "https://www.knoll.com/media/754/476/Thumb_pollock-arm-chair,4.png"}
+                   {:product-id :wassily-chair, :product-name "Wassily Chair" :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/432/603/Thumb_wassily_880,4.png"}
+                   {:product-id :risom-lounge-chair, :product-name "Risom Lounge Chair" :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/955/28/Thumb_risom-lounge-chair-880,4.png"}
+                   {:product-id :womb-chair, :product-name "Womb Chair " :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/986/679/Thumb_womb-chair-default-image,4.png"}
+                   {:product-id :womb-chair-medium, :product-name "Womb Chair - Medium" :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/539/531/Thumb_medium-womb-880,4.png"}
+                   {:product-id :womb-chair-child, :product-name "Womb Chair - Child's" :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/351/283/Thumb_childs-womb-chair-hero,4.png"}
+                   {:product-id :womb-settee, :product-name "Womb Settee" :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/775/888/Thumb_womb-settee-default-image,4.png"}
+                   ]}
+   {:product-type "Multi-Use Chairs"
+    :product-list [
+                   {:product-id :ollo-chair, :product-name "Ollo", :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/661/623/Thumb_ollo-default-thumb,4.png"}
+                   {:product-id :newson-aluminum-chair, :product-name "Newson Aluminum Chair", :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/909/711/Thumb_newson-aluminum-chair-thumbnail,4.png"}
+                   {:product-id :bertoia-molded-shell-side-chair, :product-name "Bertoia Molded Shell Side Chair", :lead-times [:std :three-week] :img-src "https://www.knoll.com/media/47/66/Thumb_Thumb_bertoia-molded-shell-chair-default,4.png"}
+                   ]}])
+
