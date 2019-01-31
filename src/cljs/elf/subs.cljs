fcfa6830f6ddda68766ad85b00dcde4c6041e1ac
Robert Houser <robcorp@mac.com>
2019-01-31 13:40:54 -0500

    Added subscriptions for lead time filters and filtered products

--- src/cljs/elf/subs.cljs
+++ src/cljs/elf/subs.cljs
@@ -1,33 +1,23 @@
 (ns elf.subs
   (:require
-   [re-frame.core :as re-frame]))
+   [re-frame.core :refer [reg-sub] :as re-frame]))
 
-(re-frame/reg-sub
+(reg-sub
  ::name
  (fn [db]
    (:name db)))
 
-(re-frame/reg-sub
+(reg-sub
  ::essentials-products
  (fn [db]
    (:essentials-products db)))
 
-(re-frame/reg-sub
- ::all-lead
+(reg-sub
+ ::filtered-products
  (fn [db]
-   (:filter-all-lead-times? db)))
+   (:filtered-products db)))
 
-(re-frame/reg-sub
- ::quick-ship
+(reg-sub
+ ::lead-time-filters
  (fn [db]
-   (:filter-quick-ship-lead-times? db)))
-
-(re-frame/reg-sub
- ::three-week-ship
- (fn [db]
-   (:filter-three-week-ship-lead-times? db)))
-
-(re-frame/reg-sub
- ::standard-ship
- (fn [db]
-   (:filter-standard-ship-lead-times? db)))
+   (:lead-time-filters db)))
