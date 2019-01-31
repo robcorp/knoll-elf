df1f73861ec09c6e9f3de1157296756a72dd4e85
Robert Houser <robcorp@mac.com>
2019-01-31 13:31:52 -0500

    Moved lead time filters into there own key in the app db.  Added filtered-products key to the app db.

--- src/cljs/elf/db.cljs
+++ src/cljs/elf/db.cljs
@@ -4,7 +4,5 @@
 (def default-db
   {:name "re-frame"
    :essentials-products dd/essentials-products
-   :filter-all-lead-times? true
-   :filter-quick-ship-lead-times? false
-   :filter-three-week-ship-lead-times? false
-   :filter-standard-ship-lead-times? false})
+   :filtered-products dd/filtered-products
+   :lead-time-filters dd/lead-time-filters})
