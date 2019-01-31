34ac307d346d63469dfa0369be9816b74e930a5c
Robert Houser <robcorp@mac.com>
2019-01-31 13:39:45 -0500

    Fully implemented handling of lead time filter checkboxes including filtering of products shown.

--- src/cljs/elf/events.cljs
+++ src/cljs/elf/events.cljs
@@ -1,25 +1,52 @@
 (ns elf.events
-  (:require
-   [re-frame.core :as re-frame]
-   [elf.db :as db]
-   [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
-   ))
+  (:require [re-frame.core :refer [reg-event-db] :as re-frame]
+            [elf.db :as db]
+            [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
+            [com.rpl.specter :refer [ALL FIRST select transform] :as spctr]))
 
-(re-frame/reg-event-db
+(reg-event-db
  ::initialize-db
  (fn-traced [_ _]
    db/default-db))
 
-(defn toggle-filter-state [db filter]
-  (assoc db filter (not (filter db))))
+(defn- lead-time-filter-value [lead-time lead-time-filters]
+  (spctr/select-first [ALL #(= (:lead-time %) lead-time) :value] lead-time-filters))
 
-(re-frame/reg-event-db
- ::check-box-clicked
- (fn [db event]
-   (let [[_ id] event]
-     #_(println id "checkbox was clicked.")
-     (cond
-       (= id "all-lead") (toggle-filter-state db :filter-all-lead-times?)
-       (= id "standard-ship") (toggle-filter-state db :filter-standard-ship-lead-times?)
-       (= id "three-week-ship") (toggle-filter-state db :filter-three-week-ship-lead-times?)
-       (= id "quick-ship") (toggle-filter-state db :filter-quick-ship-lead-times?)))))
+(defn- toggle-lead-time-filter-state [selected-filter filters]
+  (let [selected-filter-value (lead-time-filter-value selected-filter filters)
+        all-value (lead-time-filter-value :all filters)]
+    (case selected-filter
+      :all (spctr/setval [ALL :value] (not all-value) filters)
+
+      ;; default
+      (if (and all-value selected-filter-value)
+        ;; then
+        (spctr/setval [(spctr/multi-path
+                        (spctr/walker #(= :all (:lead-time %)))
+                        (spctr/walker #(= selected-filter (:lead-time %)))) :value] false filters)
+        ;; else
+        (spctr/setval [(spctr/walker #(= selected-filter (:lead-time %))) :value] (not selected-filter-value) filters)))))
+
+(defn- filter-products-by-lead-times [lead-times prods]
+  (if (empty? lead-times)
+    prods ;; return prods unfiltered
+    (select [ALL #(some lead-times (:lead-times %))] prods)))
+
+(reg-event-db
+ ::lead-time-filter-check-box-clicked
+ (fn [db [_ lead-time] event]
+   (let [updated-filters (toggle-lead-time-filter-state lead-time (:lead-time-filters db))
+         enable-all? (every? true? (select [ALL #((:lead-time %) #{:std :quick :three-week}) :value] updated-filters))
+         selected-filters (set (select [ALL #(true? (:value %)) :lead-time] updated-filters))
+         filtered-products (->> (:essentials-products db)
+                                (filter-products-by-lead-times selected-filters)
+                                (group-by :product-type))]
+     #_(println selected-filters)
+     #_(println filtered-products)
+     (assoc db
+            :lead-time-filters
+            (if enable-all?
+              (spctr/setval [(spctr/walker #(= :all (:lead-time %))) :value] true updated-filters)
+              updated-filters)
+            
+            :filtered-products filtered-products))))
