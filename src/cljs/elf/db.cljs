bb92da1a03c9b6d7be16c9dc5d555e449b64f863
Robert Houser <robcorp@mac.com>
2019-03-08 15:23:37 -0500

    Added product-category key to filters, useful for tying the filter to the filtered-products

--- src/cljs/elf/db.cljs
+++ src/cljs/elf/db.cljs
@@ -14,14 +14,20 @@
    :selected-product nil
    :lead-time-filters dd/lead-time-filters
    :ELFSeatingSelector {:description "Seating"
+                        :product-category :seatingCategories
                         :items []}
    :ELFTableSelector {:description "Tables"
+                      :product-category :tableCategories
                       :items []}
    :ELFStorageSelector {:description "Storage"
+                        :product-category :storageCategories
                         :items []}
    :ELFPowerAndDataSelector {:description "Power & Data"
+                             :product-category :powerCategories
                              :items []}
    :ELFWorkToolsSelector {:description "Work Tools & Accessories"
+                          :product-category :workToolsCategories
                           :items []}
    :ELFScreensAndBoardsSelector {:description "Screen & Communication Boards"
+                                 :product-category :screensCategories
                                  :items []}})
