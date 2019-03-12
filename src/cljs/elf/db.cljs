cfab8e51c924e6d6a0cd1a4acdceffdd15170991
Robert Houser <robcorp@mac.com>
2019-03-12 15:24:31 -0400

    Modified default-db to try to load all-products and filtered-products from localStorage.
    Also try to load filters from localStorage, if not found, then use
    hardcoded defaults.

--- src/cljs/elf/db.cljs
+++ src/cljs/elf/db.cljs
@@ -4,8 +4,8 @@
 (def default-db
   {:name "Knoll Essentials Lead Times & Finishes"
    :loading-all-products true
-   :all-products []
-   :filtered-products nil
+   :all-products (cljs.reader/read-string (.getItem js/localStorage "all-products"))
+   :filtered-products (cljs.reader/read-string (.getItem js/localStorage "all-products"))
    :filtered-seating-products {}
    :filtered-table-products {}
    :filtered-storage-products {}
@@ -14,21 +14,64 @@
    :filtered-screen-products {}
    :selected-product nil
    :lead-time-filters dd/lead-time-filters
-   :ELFSeatingSelector {:description "Seating"
-                        :product-category :seatingCategories
-                        :items []}
-   :ELFTableSelector {:description "Tables"
-                      :product-category :tableCategories
-                      :items []}
-   :ELFStorageSelector {:description "Storage"
-                        :product-category :storageCategories
-                        :items []}
-   :ELFPowerAndDataSelector {:description "Power & Data"
-                             :product-category :powerCategories
-                             :items []}
-   :ELFWorkToolsSelector {:description "Work Tools & Accessories"
-                          :product-category :workToolsCategories
-                          :items []}
-   :ELFScreensAndBoardsSelector {:description "Screen & Communication Boards"
-                                 :product-category :screensCategories
-                                 :items []}})
+   :ELFSeatingSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFSeatingSelector"))]
+                         from-local-storage
+                         {:name "ELFSeatingSelector"
+                          :description "Seating-"
+                          :product-category :seatingCategories
+                          :items [{:label "All" :value false}
+                                  {:label "Work" :value false}
+                                  {:label "Side and Multi-Use" :value false}
+                                  {:label "Lounge" :value false}
+                                  {:label "Stools" :value false}
+                                  {:label "Outdoor" :value false}]})
+   :ELFTableSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFTableSelector"))]
+                       from-local-storage
+                       {:name "ELFTableSelector"
+                        :description "Tables-"
+                        :product-category :tableCategories
+                        :items [{:label "All" :value false}
+                                {:label "Meeting & Conference" :value false}
+                                {:label "Desks & Benching" :value false}
+                                {:label "Height-Adjustable" :value false}
+                                {:label "Dining & Multi-use" :value false}
+                                {:label "Training" :value false}
+                                {:label "Side & Coffee" :value false}
+                                {:label "Outdoor" :value false}]})
+   :ELFStorageSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFStorageSelector"))]
+                         from-local-storage
+                         {:name "ELFStorageSelector"
+                          :description "Storage-"
+                          :product-category :storageCategories
+                          :items [{:label "All" :value false}
+                                  {:label "Pedestals" :value false}
+                                  {:label "Doublewide Pedestals" :value false}
+                                  {:label "Credenzas Carts and Consoles" :value false}
+                                  {:label "Lockers and Wardrobes" :value false}
+                                  {:label "Towers" :value false}
+                                  {:label "Mounted Storage" :value false}]})
+   :ELFPowerAndDataSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFPowerAndDataSelector"))]
+                              from-local-storage
+                              {:name "ELFPowerAndDataSelector"
+                               :description "Enhanced Power And Data-"
+                               :product-category :powerCategories
+                               :items [{:label "All" :value false}
+                                       {:label "Power & Technology Distribution" :value false}
+                                       {:label "Interior Architecture" :value false}]})
+   :ELFWorkToolsSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFWorkToolsSelector"))]
+                           from-local-storage
+                           {:name "ELFWorkToolsSelector"
+                            :description "Accessories & Work Tools-"
+                            :product-category :workToolsCategories
+                            :items [{:label "All" :value false}
+                                    {:label "Lighting" :value false}
+                                    {:label "Technology Support" :value false}
+                                    {:label "Desktop Management" :value false}]})
+   :ELFScreensAndBoardsSelector (if-let [from-local-storage (cljs.reader/read-string (.getItem js.localStorage "ELFScreensAndBoardsSelector"))]
+                                  from-local-storage
+                                  {:name "ELFScreensAndBoardsSelector"
+                                   :description "Screens & Communication Boards-"
+                                   :product-category :screensCategories
+                                   :items [{:label "All" :value false}
+                                           {:label "Screens & Dividers" :value false}
+                                           {:label "Communication Boards" :value false}]})})
