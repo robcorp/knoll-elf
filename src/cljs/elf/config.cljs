1817e0913c8bf0378d4fe5a2246ba09dfe85d4eb
Robert Houser <robcorp@mac.com>
2019-03-26 11:57:15 -0400

    Added flag for controlling whether to use the local all-products.json or make an Ajax call to the staging server

--- src/cljs/elf/config.cljs
+++ src/cljs/elf/config.cljs
@@ -2,3 +2,7 @@
 
 (def debug?
   ^boolean goog.DEBUG)
+
+;; When debug? is true, this flag determines whether to use the local all-products.json file or
+;; to use http://knlprdwcsmgt1.knoll.com/cs/Satellite?pagename=Knoll/Common/Utils/EssentialsPopupProductsJSON
+(def use-local-products? true)
